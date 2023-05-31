eae8af098a2c8e00364b507b5a58524a5a0e8654
Chaitanya <Chaitanya@LAPTOP-SREM8SL9>
2023-05-31 20:10:23 +0530

    here i commit 1st time

---
+++ src/main/java/com/mycompany/javaprognetbeans/JavaProgNetBeans.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javaprognetbeans;
+
+/**
+ *
+ * @author Chaitanya
+ */
+public class JavaProgNetBeans {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
